102a0c7aa91c91ad65aae238bbdea5049a7052b7
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-12-10 18:17:42 -0500

    Refactor playfair.rs

--- src/playfair.rs
+++ src/playfair.rs
@@ -1,18 +1,22 @@
 // A simple implementation of the Playfair cipher
-use itertools::Itertools;
 use collect_slice::CollectSlice;
+use itertools::Itertools;
 
 /// Generates the corresponding Playfair key sequence from a keyword.
-/// If J is included, 
+/// If J is included,
 /// The key is a particular ordering of the alphabet, where the key comes first (except J)
-fn playfair_matrix(keyword: &str) -> Box<[char; 25]> {
-    if !keyword.chars().all(|c| ('A'..'Z').contains(&c)) {
+pub fn generate_key(keyword: &str, omit: char) -> Box<[char; 25]> {
+    if !keyword.chars().all(|c| ('A'..='Z').contains(&c)) {
         panic!("Keys must be upper-alpha.");
     }
     let mut out = Box::new([' '; 25]);
-    keyword.chars()
-        .map(|c| if c == 'J' { 'I' } else { c }) // replace J with I
-        .chain("ABCDEFGHIKLMNOPQRSTUVWXYZ".chars()) // append alphabet
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".replace(omit, "");
+    keyword
+        .chars()
+        .map(|c| if c == omit {
+            panic!("Keyword cannot contain the ommited char.");
+        } else { c }) // replace J with I
+        .chain(alphabet.chars()) // append alphabet
         .unique() // remove dupes
         .collect_slice_checked(&mut *out);
     out
@@ -45,19 +49,23 @@ fn playfair_crypt_pair(a: char, b: char, key: &[char; 25], decrypt: bool) -> (ch
     };
     return (key[a.0 * 5 + a.1], key[b.0 * 5 + b.1]); // convert new coords
 }
-fn crypt(text: &str, key: &str, decrypt: bool) -> String {
+fn crypt(text: &str, key: &str, decrypt: bool, omit: char) -> String {
     // Error checking
-    text.chars().for_each(|c| { if !('A'..='Z').contains(&c) {
-        panic!("Text must be strictly upper-alpha (A-Z). Found '{}'.", c);
-    }});
-    text.chars().tuples::<(_, _)>().for_each(|(a, b)| { if a == b {
-        panic!("Text must not contain repeated chars. Found '{a}{b}'. Try '{a}X{b}'.");
-    }});
+    text.chars().for_each(|c| {
+        if !('A'..='Z').contains(&c) {
+            panic!("Text must be strictly upper-alpha (A-Z). Found '{}'.", c);
+        }
+    });
+    text.chars().tuples::<(_, _)>().for_each(|(a, b)| {
+        if a == b {
+            panic!("Text must not contain repeated chars. Found '{a}{b}'. Try '{a}X{b}'.");
+        }
+    });
     let mut text = text.clone().to_string();
     if text.len() % 2 != 0 {
         text.push('Z');
     }
-    let key = playfair_matrix(key);
+    let key = generate_key(key, omit);
     text.chars()
         .tuples::<(_, _)>()
         .flat_map(|(a, b)| {
@@ -68,16 +76,40 @@ fn crypt(text: &str, key: &str, decrypt: bool) -> String {
 }
 
 /// Encrypt the plaintext text with the given key, using the Playfair cipher.
-/// 
-/// The key and text must consist of 
+///
+/// The key and text must consist of
 /// The text must consist of an even number of characters, with none repeated.
 /// Panics otherwise.
-pub fn encrypt(text: &str, key: &str) -> String {
-    return crypt(text, key, false);
+pub fn encrypt(text: &str, key: &str, omit: char) -> String {
+    return crypt(text, key, false, omit);
 }
 /// Decrypts the plaintext text with the given key, using the Playfair cipher.
 ///
 /// Panics if the input is not all uppercase alphabetical characters.
-pub fn decrypt(text: &str, key: &str) -> String {
-    return crypt(text, key, true);
+pub fn decrypt(text: &str, key: &str, omit: char) -> String {
+    return crypt(text, key, true, omit);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_generate_key() {
+        // combine J and K
+        let key: String = generate_key("INTERCEPTOR", 'K').iter().collect();
+        assert_eq!(key, "INTERCPOABDFGHJLMQSUVWXYZ");
+    }
+
+    #[test]
+    fn test_encrypt() {
+        let result = encrypt("ZAPXPHOD", "ZERO", 'J');
+        assert_eq!(result, "EZSVNIRF");
+    }
+    
+    #[test]
+    fn test_decrypt() {
+        let result = decrypt("EZSVNIRF", "ZERO", 'J');
+        assert_eq!(result, "ZAPXPHOD");
+    }
 }
